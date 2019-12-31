33d9296130a091e9e137a8b78feb5248c771d558
League Student <league@iMac-18.attlocal.net>
2019-12-31 14:56:21 -0800

    dsjndjBNDhebfh fWS hfuyegfyuwegfuywggYEDFED YGFYWET FUWETGD66FeDÏ6YE´cg´µ≤≤≤≤≤≤≤≤≤≤≤≤≤≤≤≤≤≤≤≤
    xcScd

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -3,38 +3,51 @@ package section2;
 import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
+import javafx.scene.paint.Color;
+
 public class FourSquare {
 	
 	// 2. Create a new Robot
-
+Robot boo=new Robot();
 	void go() {
 		// 4. Make the robot move as fast as possible
-
-		// 5. Set the pen width to 5
+boo.setSpeed(30);
+boo.penDown();
+boo.setPenColor(java.awt.Color.CYAN);		// 5. Set the pen width to 5
 
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
+for (int i = 0; i < 4; i++) {
+	
 
 			// 7. Set the pen color to random
 	
 			// 1. Call the drawSquare() method
+		drawSquare();
 	
 			// 8. Turn the robot 90 degrees to the right
-
-	}
+boo.turn (90);
+	}}
 
 	
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
-		
-		
+boo.turn(90);		
+	boo. move(100);	
+	boo.turn(90);		
+	boo. move(100);
+	boo.turn(90);		
+	boo. move(100);	
+	boo.turn(90);		
+	boo. move(100);	
+	boo. move(100);	
+	boo. move(100);	
+	boo.turn(180);	
 	}
-
 	public static void main(String[] args) {
 		new FourSquare().go();
 	}
 
 }
-
 
 
--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -1,25 +1,31 @@
 package section2;
 
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		Robot mario=new Robot();
 		// 5. Set your robot's pen down 
-		
-		// 3. Set the robot to go at max speed (100)
-		
+		mario.penDown();
+		//3. Set the robot to go at max speed (100)
+		mario.setSpeed(100);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
-		
+		for (int i = 0;i< 75; i++) {
+			mario.setRandomPenColor();
+		mario.changeRobot("https://mario.nintendo.com/assets/img/home/intro/mario-pose2.png");
 			// 7. Change the pen color to random
 		
 			// 6. Move the robot 5 times the loop counter (5*i)
-			
+		mario.move(5*i);
 			// 2. Turn the robot 360/7 degrees to the right
-		
-			// 8. Set the pen width to i
+		mario.turn(360/7);
+			// 8. Set the pen width to i 
+		mario.setPenWidth(i);
+		}
 			
 	}
 }
