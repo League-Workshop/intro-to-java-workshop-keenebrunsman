ca56c97274df3d32182c129e128694bc30f357ce
League Student <league@iMac-18.attlocal.net>
2020-01-01 14:57:13 -0800

    pretty dark
    It is a dark canday cwush

--- src/section3/CandyCrush.java
+++ src/section3/CandyCrush.java
@@ -6,13 +6,15 @@ public class CandyCrush {
 
 	public static void main(String[] args) {
 		// 1. Using a pop-up, ask the user who they don't like
-
+String name=JOptionPane.showInputDialog(null, "who do you despise, to the depths of your being?");
 		// 2. Change the next line so that the pop-up will tell them they have a crush on that person
-		JOptionPane.showMessageDialog(null, "You totally have a crush on Java!");
+		JOptionPane.showMessageDialog(null, "You must destroy them, for they  be a foul beast, destroy them, I say! DESTROY THEM! "+name+"!");
 		// 3. Ask the user for the name of their best friend
-
+		String name1=JOptionPane.showInputDialog(null, "who will you trust with your life, no thought of the cost?");
+				
 		// 4. Tell them their best friend is as sweet as candy
-
+		JOptionPane.showMessageDialog(null, "Shall you murder them, for they are most likley an ENEMY SPY! kill them, and feel.no. PAIN!");
+		
 	} 
 }
 
--- src/section3/MadlibsOfTheAmazon.java
+++ src/section3/MadlibsOfTheAmazon.java
@@ -7,17 +7,16 @@ public class MadlibsOfTheAmazon {
 	public static void main(String[] args) {
 		// Put this sentence in a pop up:
 		// If you find yourself having to cross a piranha-infested river, here's how to do it...
-		
-		// Get the user to enter an adjective
-
+				// Get the user to enter an adjective
+		String name=JOptionPane.showInputDialog(null,"what is the most terrible adjitive you can think of?");
 		// Get the user to enter a type of liquid
-
+		String wwww=JOptionPane.showInputDialog(null, "what is the most disgusting liquid in the universe");
 		// Get the user to enter a body part
-
+		String qgggg=JOptionPane.showInputDialog(null, "what is da most disgusting bodE part uf yo body?"); 
 		// Get the user to enter a verb
-
-		// Get the user to enter a place
-
+		String thingthingy=JOptionPane.showInputDialog(null,  "what do you think is the most prededeor-attracting thing to do?");          
+				// Get the user to enter a place
+		String thing=JOptionPane.showInputDialog(null,  "what is your least favorite place to go?");
 		// Fit the user's words into this sentence, and save it in a String:
 		// Piranhas are more [adjective] during the day, so cross the river at
 		// night. Piranhas are attracted to fresh [type of liquid] and will most
@@ -25,9 +24,13 @@ public class MadlibsOfTheAmazon {
 		// you do, if you have an open wound, try to find another way to get
 		// back to the [place]. Good luck!
 		
-		// Make a pop-up for the final story. You can use \n to go to the next line
 		
-
+		String cookie= "Piranhas are more   "+name+ "   during the day, so cross the river at"+
+		 "night. Piranhas are attracted  to fresh  " +wwww+ "  and will most"+
+		 "likely take a bite out of your if you   " +thingthingy+ "Whatever."   +
+		 "you do, if you have an open wound, try to find another way to get"+
+		 "back to the"    +thing+   "Good luck!";
+		JOptionPane.showMessageDialog(null,(cookie));
 	}
 }
 
